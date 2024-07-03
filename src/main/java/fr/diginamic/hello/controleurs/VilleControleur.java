fe751f4efe9cd20cb7e83a407fc0f57064e8eedd
lysiandon <don.lysiane@gmail.com>
2024-07-03 16:06:46 +0200

    finished exo 5

--- src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
+++ src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
@@ -2,10 +2,12 @@ package fr.diginamic.hello.controleurs;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import fr.diginamic.hello.classes.Ville;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,16 +43,53 @@ public class VilleControleur {
 	}
 
 	@PostMapping
-	public ResponseEntity<String> addVille(@RequestBody Ville body) {
+	public ResponseEntity<?> addVille(@RequestBody Ville body) {
 
 		boolean villeExists = villes.stream().anyMatch(ville -> ville.getNom().equalsIgnoreCase(body.getNom()));
+		boolean idExists = villes.stream().anyMatch(ville -> ville.getId() == body.getId());
 
 		if (villeExists) {
 			return ResponseEntity.badRequest().body("La ville existe déjà");
 		}
+
+		if (idExists) {
+			return ResponseEntity.badRequest().body("Cet id existe déjà");
+		}
+
 		villes.add(body);
 		return ResponseEntity.ok("Ville insérée avec succès");
 
+	}
+
+	@PutMapping(path = "/{id}")
+	public ResponseEntity<?> updateVille(@RequestBody Ville body, @PathVariable int id) {
+
+		Optional<Ville> optionalVille = villes.stream().filter(v -> v.getId() == id).findFirst();
+
+		if (!optionalVille.isPresent()) {
+			return ResponseEntity.badRequest().body("Cet id n'existe pas");
+		}
+
+		Ville updatedVille = optionalVille.get();
+		System.out.println(updatedVille);
+		updatedVille.setNom(body.getNom());
+		updatedVille.setNbHabitants(body.getNbHabitants());
+
+		return ResponseEntity.ok(updatedVille);
+	}
+
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<?> deleteVille(@PathVariable int id) {
+
+		Optional<Ville> optionalVille = villes.stream().filter(v -> v.getId() == id).findFirst();
+
+		if (!optionalVille.isPresent()) {
+			return ResponseEntity.badRequest().body("Cet id n'existe pas");
+		}
+		
+		villes.remove(optionalVille.get());
+
+		return ResponseEntity.ok("ville correctement supprimée");
 	}
 
 }
