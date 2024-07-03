7bee87fe523a37f21268a0029c0eb3168cf14d24
lysiandon <don.lysiane@gmail.com>
2024-07-03 12:04:39 +0200

    finished exo 2

--- src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
+++ src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
@@ -1,15 +1,23 @@
 package fr.diginamic.hello.controleurs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.diginamic.hello.services.HelloService;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloControleur {
+	@Autowired
+	private HelloService helloService;
 
 	@GetMapping
 	public String direHello() {
-		return "Hello :)";
+		return helloService.salutations();
 	}
+	
+	
+	
 }
