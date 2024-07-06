1126bd009b8d58dd7d89b4bf8536ea39a0f1d7be
lysiandon <don.lysiane@gmail.com>
2024-07-06 11:42:52 +0200

    refacto

--- src/main/java/fr/diginamic/hello/services/DepartementService.java
+++ src/main/java/fr/diginamic/hello/services/DepartementService.java
@@ -1,59 +1,90 @@
 package fr.diginamic.hello.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import fr.diginamic.hello.classes.Departement;
+import fr.diginamic.hello.entities.Departement;
+import fr.diginamic.hello.entities.Region;
 import fr.diginamic.hello.repositories.DepartementRepository;
+import fr.diginamic.hello.repositories.RegionRepository;
 import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class DepartementService {
 
 	@Autowired
-	private DepartementRepository departRepo;
+	private DepartementRepository departementRepository;
 
-	@Transactional
+	@Autowired
+	private RegionRepository regionRepository;
+
+	@Transactional(readOnly = true)
 	public List<Departement> extractDepartements() {
-		return (List<Departement>) departRepo.findAll();
+		return (List<Departement>) departementRepository.findAll();
 	}
 
-	@Transactional
+	@Transactional(readOnly = true)
 	public Departement extractDepartement(int id) {
-		return departRepo.findById(id)
+		return departementRepository.findById(id)
 				.orElseThrow(() -> new EntityNotFoundException("Departement non trouvé avec ID : " + id));
 	}
 
-	@Transactional
-	public Departement extractDepartement(String nom) {
-		return departRepo.findByNom(nom)
-				.orElseThrow(() -> new EntityNotFoundException("Departement non trouvé avec nom : " + nom));
+	@Transactional(readOnly = true)
+	public Departement extractDepartement(String code) {
+		return departementRepository.findByCode(code)
+				.orElseThrow(() -> new EntityNotFoundException("Departement non trouvé avec code : " + code));
 	}
 
 	@Transactional
-	public Departement insertDepartement(Departement departement) {
-		return departRepo.save(departement);
+	public Departement createDepartement(Departement departement) {
+		boolean exists = departementRepository.existsByCode(departement.getCode());
+		if (exists) {
+			throw new IllegalArgumentException(
+					"Le département avec le code " + departement.getCode() + " existe déjà.");
+		}
+
+		Region region = departement.getRegion();
+		if (region != null) {
+			Optional<Region> existingRegion = regionRepository.findById(region.getId());
+			if (existingRegion.isPresent()) {
+				departement.setRegion(existingRegion.get());
+			} else {
+				throw new IllegalArgumentException("La région spécifiée n'existe pas.");
+			}
+		}
+		return departementRepository.save(departement);
 	}
 
 	@Transactional
-	public Departement modifierDepartement(int id, Departement dptm) {
-		return departRepo.findById(id).map(d -> {
-			d.setNom(dptm.getNom());
-			d.setVilles(dptm.getVilles());
-			return departRepo.save(d);
+	public Departement updateDepartement(int id, Departement dptm) {
+		return departementRepository.findById(id).map(d -> {
+			d.setCode(dptm.getCode());
+			d.setRegion(dptm.getRegion());
+			return departementRepository.save(d);
 		}).orElseThrow(() -> new EntityNotFoundException("Departement non trouvé avec ID : " + id));
 
 	}
 
-	public void supprimerDepartement(int id) {
-		if (!departRepo.existsById(id)) {
+	@Transactional
+	public void deleteDepartement(int id) {
+		if (!departementRepository.existsById(id)) {
 			throw new EntityNotFoundException("Departement non trouvé avec ID : " + id);
 		}
-		departRepo.deleteById(id);
+		departementRepository.deleteById(id);
 	}
-	
+
+	@Transactional(readOnly = true)
+	public Departement getDepartementByCode(String code) {
+		return departementRepository.findByCode(code).orElseThrow(
+				() -> new IllegalArgumentException("Le département avec le code " + code + " n'existe pas."));
+	}
+
+	public boolean existsByCode(String codeDepartement) {
+		return departementRepository.existsByCode(codeDepartement);
+	}
 
 }
--- src/main/java/fr/diginamic/hello/services/VilleService.java
+++ src/main/java/fr/diginamic/hello/services/VilleService.java
@@ -1,12 +1,15 @@
 package fr.diginamic.hello.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import fr.diginamic.hello.classes.Departement;
-import fr.diginamic.hello.classes.Ville;
+import fr.diginamic.hello.entities.Departement;
+import fr.diginamic.hello.entities.Ville;
+import fr.diginamic.hello.exceptions.EntityException;
 import fr.diginamic.hello.repositories.DepartementRepository;
 import fr.diginamic.hello.repositories.VilleRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -19,25 +22,47 @@ public class VilleService {
 
 	@Autowired
 	private VilleRepository villeRepository;
-	
+
 	@Autowired
 	private DepartementRepository departementRepository;
 
-	public List<Ville> extractVilles() {
-		return (List<Ville>) villeRepository.findAll();
+//	@Autowired
+//	private EntityException EntityException;
+
+	@Transactional(readOnly = true)
+	public Page<Ville> extractVilles(Pageable pageable) {
+	    return villeRepository.findAll(pageable);
 	}
 
+	@Transactional(readOnly = true)
 	public Ville extractVille(int idVille) {
 		return villeRepository.findById(idVille)
 				.orElseThrow(() -> new EntityNotFoundException("Ville non trouvée avec ID : " + idVille));
 	}
 
+	@Transactional(readOnly = true)
 	public Ville extractVille(String nom) {
 		return villeRepository.findByNom(nom)
 				.orElseThrow(() -> new EntityNotFoundException("Ville non trouvée avec nom : " + nom));
 	}
 
-	public Ville insertVille(Ville ville) {
+	@Transactional
+	public Ville createVille(Ville ville) {
+		boolean villeExists = villeRepository.existsByNomAndDepartement(ville.getNom(), ville.getDepartement());
+		boolean departementExists = ville.getDepartement() != null
+				&& departementRepository.existsById(ville.getDepartement().getId());
+
+		if (villeExists) {
+			throw new EntityException("La ville existe déjà.");
+		}
+
+		if (!departementExists) {
+			throw new IllegalArgumentException("Département non renseigné ou introuvable.");
+		}
+
+		Departement departement = departementRepository.findById(ville.getDepartement().getId()).get();
+		ville.setDepartement(departement);
+
 		return villeRepository.save(ville);
 	}
 
@@ -57,18 +82,67 @@ public class VilleService {
 		}
 		villeRepository.deleteById(idVille);
 	}
-	
-	   public List<Ville> findTopNVillesByDepartement(int departementId, int maxResults) {
-	        Optional<Departement> departementOpt = departementRepository.findById(departementId);
-	        if (departementOpt.isPresent()) {
-	            Departement departement = departementOpt.get();
-	            PageRequest pageRequest = PageRequest.of(0, maxResults);
-	            return villeRepository.findByDepartementOrderByNbHabitantsDesc(departement, pageRequest);
-	        }
-	        return null;
-	    }
-	   
-	   public List<Ville> findVillesByPopulationAndDepartement(int minPopulation, int maxPopulation, Departement departement) {
-	        return villeRepository.findByNbHabitantsBetweenAndDepartement(minPopulation, maxPopulation, departement);
-	    }
+
+	@Transactional(readOnly = true)
+	public List<Ville> findTopNVillesByDepartement(int departementId, int maxResults) {
+		Optional<Departement> departementOpt = departementRepository.findById(departementId);
+		if (departementOpt.isPresent()) {
+			Departement departement = departementOpt.get();
+			PageRequest pageRequest = PageRequest.of(0, maxResults);
+			return villeRepository.findByDepartementOrderByNbHabitantsDesc(departement, pageRequest);
+		}
+		return null;
+	}
+
+	@Transactional(readOnly = true)
+	public List<Ville> findVillesByPopulationAndDepartement(int minPopulation, int maxPopulation,
+			Departement departement) {
+		return villeRepository.findByNbHabitantsBetweenAndDepartement(minPopulation, maxPopulation, departement);
+	}
+
+	@Transactional(readOnly = true)
+	public boolean existsByNom(String nom) {
+		return villeRepository.existsByNom(nom);
+	}
+
+	// Recherche de toutes les villes dont le nom commence par une chaine de
+	// caractères
+	@Transactional(readOnly = true)
+	public List<Ville> findByNomStartingWith(String prefix) {
+		return villeRepository.findByNomStartingWith(prefix);
+	}
+
+	// Recherche de toutes les villes dont la population est supérieure à min
+	@Transactional(readOnly = true)
+	public List<Ville> findByNbHabitantsGreaterThan(int minPopulation){
+		return villeRepository.findByNbHabitantsGreaterThan(minPopulation);
+	}
+
+	// Recherche de toutes les villes dont la population est supérieure à min et
+	// inférieure à max
+	@Transactional(readOnly = true)
+	public List<Ville> findByNbHabitantsBetween(int minPopulation, int maxPopulation){
+		return villeRepository.findByNbHabitantsBetween(minPopulation, maxPopulation);
+	}
+
+	// Recherche de toutes les villes d’un département dont la population est
+	// supérieure à min
+	@Transactional(readOnly = true)
+	public List<Ville> findByDepartementAndNbHabitantsGreaterThan(Departement departement, int minPopulation){
+		return villeRepository.findByDepartementAndNbHabitantsGreaterThan(departement, minPopulation);
+	}
+
+	// Recherche de toutes les villes d’un département dont la population est
+	// supérieure à min et inférieure à max
+	@Transactional(readOnly = true)
+	public List<Ville> findByDepartementAndNbHabitantsBetween(Departement departement, int minPopulation, int maxPopulation){
+		return villeRepository.findByDepartementAndNbHabitantsBetween(departement, minPopulation, maxPopulation);
+	}
+
+	// Recherche des n villes les plus peuplées d’un département donné
+	@Transactional(readOnly = true)
+	public List<Ville> findTopNVillesByDepartementOrderByNbHabitantsDesc(Departement departement, Pageable pageable){
+		return villeRepository.findTopNVillesByDepartementOrderByNbHabitantsDesc(departement, pageable);
+	}
+
 }
